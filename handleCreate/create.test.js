ae2f26a430c854573ead1b0eccb783757223a0b9
Adnan Mohamud <adnanmohamud6@gmail.com>
2023-10-12 15:25:56 -0700

    added in deployed link

--- handleCreate/create.test.js
+++ handleCreate/create.test.js
@@ -6,12 +6,13 @@ describe('Testing the createNbaTeam lambda', () => {
   it('should create a new Nba team', async () => {
     const event = {
       body: {
-        id: 4,
+        id: 1, // Provide a valid ID
         name: 'Boston Celtics',
         conference: 'East',
         color: 'Green',
       },
     };
+  console.log('checking to see if EVENT BODY ID EXISTS', event.body.id)
 
     const result = await createNbaTeam.handler(event);
 
@@ -19,7 +20,7 @@ describe('Testing the createNbaTeam lambda', () => {
     expect(typeof result.body).toBe('string');
 
     const body = JSON.parse(result.body);
-    expect(body).toHaveProperty('id', 4);
+    expect(body).toHaveProperty('id', 1);
     expect(body).toHaveProperty('name', 'Boston Celtics');
     expect(body).toHaveProperty('conference', 'East');
     expect(body).toHaveProperty('color', 'Green');
